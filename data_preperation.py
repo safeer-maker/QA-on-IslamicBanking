0ecd6dcdebff5c95527f2ec99ccc0ffcb5582917
Safeer Linux <bsee15055@itu.edu.pk>
2024-06-25 13:12:54 +0500

    working on data preperation

---
+++ data_preperation.py
@@ -0,0 +1,8 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from data_extraction import data_extraction
+
+class data_preperation (data_extraction):
+    def __init__ ():
+        super().__init__()
+        pass
